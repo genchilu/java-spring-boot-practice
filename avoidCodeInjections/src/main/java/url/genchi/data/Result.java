deed09d95c8495bf253d2d07c942bbd0a04c4380
Genchi <genchilu@gmail.com>
2016-04-07 23:43:42 +0800

    Add code injection example

---
+++ avoidCodeInjections/src/main/java/url/genchi/data/Result.java
@@ -0,0 +1,17 @@
+package url.genchi.data;
+
+public class Result {
+    private String result;
+
+    public Result(String result) {
+        this.result = result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getResult(){
+        return result;
+    }
+}
